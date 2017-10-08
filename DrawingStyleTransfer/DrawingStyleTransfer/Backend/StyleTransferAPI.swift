946780b2c1bc8b8b3ce9d74f5baf2d7d13a695f9
Torgeir Lien <torgeir.lien@corentium.com>
2017-10-08 15:05:54 +0200

    Added AlamoFIre through podfiles

---
+++ DrawingStyleTransfer/DrawingStyleTransfer/Backend/StyleTransferAPI.swift
@@ -0,0 +1,9 @@
+//
+//  StyleTransferAPI.swift
+//  DrawingStyleTransfer
+//
+//  Created by Torgeir Lien on 08/10/2017.
+//  Copyright © 2017 Torgeir Lien. All rights reserved.
+//
+
+import Foundation
