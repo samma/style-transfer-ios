0a6d9f0409830510f4be377a279a8326efc06023
Torgeir Lien <torgeir.lien@corentium.com>
2017-10-10 21:34:09 +0200

    Now able to convert images using the REST api. Tests running fine.

--- DrawingStyleTransfer/DrawingStyleTransfer/Backend/StyleTransferAPIDeepArtImpl.swift
+++ DrawingStyleTransfer/DrawingStyleTransfer/Backend/StyleTransferAPIDeepArtImpl.swift
@@ -19,13 +19,48 @@ struct DeepArtStyleResponse {
 
 class StyleTransferAPIDeepArtImpl : StyleTransferAPIProtocol {
     let styleEndPoint = URL(string: "https://turbo.deepart.io/styles/")
+    let uploadEndPoint = URL(string: "https://turbo.deepart.io/api/post/")
+    let downloadEndPoint = "https://turbo.deepart.io/media/output/"
+
+    func convertImageWithStyle(image: UIImage, style: Style, completion: @escaping (Error?, URL?) -> Void) {
+        
+        let imageData = UIImageJPEGRepresentation(image, 1)
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(style.index.data(using: String.Encoding.utf8)!, withName: "style")
+            multipartFormData.append(imageData!, withName: "input_image", fileName: "input_image", mimeType: "image/jpeg")
+        }, to:uploadEndPoint!)
+        { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                
+                /*upload.uploadProgress(closure: { (Progress) in
+                    print("Upload Progress: \(Progress.fractionCompleted)")
+                })*/
+                
+                upload.responseData { response in
+                    let myImageId = String(bytes: response.data!, encoding: String.Encoding.utf8)
+                    completion(nil, self.makeDownloadURL(id: myImageId!))
+                }
+            case .failure(let encodingError):
+                completion(encodingError, nil)
+            }
+        }
+    }
     
+    internal func makeDownloadURL(id: String) -> URL? {
+        return URL(string: (self.downloadEndPoint + id + ".jpg"))
+    }
     func getStyles(maxNum: Int, completion: @escaping (Error?, [Style]) -> Void) {
         
         self.downloadStyleDef() { styleResponses in
             var stylesList:[Style] = []
             let myGroup = DispatchGroup()
             for (index, styleResponse) in styleResponses.enumerated() {
+                if (index >= maxNum) {
+                    break
+                }
+                myGroup.enter()
                 Alamofire.request(styleResponse.location).responseImage { response in
                     if let image = response.result.value {
                         let oneStyle = Style(index: styleResponse.id, image: image, name: styleResponse.name)
@@ -33,10 +68,6 @@ class StyleTransferAPIDeepArtImpl : StyleTransferAPIProtocol {
                         print("image downloaded: \(image)")
                     }
                     myGroup.leave()
-                }
-
-                if (index > maxNum) {
-                    break
                 }
             }
             
--- DrawingStyleTransfer/DrawingStyleTransferTests/StyleTransferAPIDeepArtImplTest.swift
+++ DrawingStyleTransfer/DrawingStyleTransferTests/StyleTransferAPIDeepArtImplTest.swift
@@ -9,6 +9,31 @@
 import XCTest
 
 class StyleTransferAPIDeepArtImplTest: XCTestCase {
+    
+    func testUploadImage() {
+        let apiGateway = StyleTransferAPIDeepArtImpl()
+        let expect = expectation(description: "Uloading some image")
+        
+        
+        let image = UIImage(named: "mountain.png", in: Bundle(for: StyleTransferAPIDeepArtImplTest.self), compatibleWith: nil)
+        let style = Style(index: "1", image: image!, name: "whatever")
+        
+        apiGateway.convertImageWithStyle(image: image!, style: style) { (e, url) in
+            if e != nil {
+                XCTFail()
+            }
+            if (url == nil) {
+                XCTFail()
+            }
+            print("The URL is: \(url!)")
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10) { error in
+            
+        }
+
+    }
 
     func testGetStyleDef() {
         let apiGateway = StyleTransferAPIDeepArtImpl()
@@ -32,7 +57,7 @@ class StyleTransferAPIDeepArtImplTest: XCTestCase {
         let expect2 = expectation(description: "Downloading styles")
         
         apiGateway.getStyles(maxNum: 5) { error, styles in
-            XCTAssert(styles.count == 0)
+            XCTAssertEqual(styles.count, 5)
             XCTAssert(error == nil)
             expect2.fulfill()
         }
