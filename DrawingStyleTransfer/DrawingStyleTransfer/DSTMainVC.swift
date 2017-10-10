e1ac2f955b15828033e90ebe868c536555fe131d
Torgeir Lien <torgeir.lien@corentium.com>
2017-10-10 22:37:42 +0200

    Baby steps towards making the UI

--- DrawingStyleTransfer/DrawingStyleTransfer/DSTMainVC.swift
+++ DrawingStyleTransfer/DrawingStyleTransfer/DSTMainVC.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DSTMainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
